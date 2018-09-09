7ca0c3f3cc08bef1ada4785696371ff7948c4e43
jormabs <jormabs@hotmail.com>
2018-09-09 14:27:41 -0500

    03 - Vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  VCard,
+  VImg,
+  transitions
+} from 'vuetify'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    VCard,
+    VImg,
+    transitions
+  },
+  theme: {
+    primary: '#FFC952',
+    secondary: '#a851f1',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  },
+})
